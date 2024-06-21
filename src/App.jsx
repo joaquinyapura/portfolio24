9c73ebfe4820d8b687446cf9d82695373c7d717f
Joaquin <joaquinyapura@gmail.com>
2024-06-21 18:44:41 -0300

    actualización de wave a la img avatar

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,21 @@ import tw from "./assets/tw.png";
 
 function App() {
   return (
-    <div className="w-screen h-screen  absolute">
+    <div className="w-screen h-screen ">
       <div className="w-full">
-        <nav className=" w-full justify-between py-4 px-40 bg-white text-gray-500">
+        <nav className=" w-full justify-between py-4 px-40 bg-white text-gray-500 text-xl">
           <ul className="flex gap-4">
             <li>
-              <a href="">LINK</a>
+              <a href="">Inicio</a>
             </li>
             <li>
-              <a href="">LINK</a>
+              <a href="">Acerca de mi</a>
             </li>
             <li>
-              <a href="">LINK</a>
+              <a href="">Portfolio</a>
             </li>
             <li>
-              <a href="">LINK</a>
+              <a href="">Contacto</a>
             </li>
             <div className="flex gap-4">
               <span>
@@ -42,18 +42,18 @@ function App() {
           </ul>
         </nav>
 
-        <div className="hero flex w-full px-40">
-          <div className="textos flex flex-col w-1/2 mt-20">
+        <div className="hero flex w-full px-40 relative">
+          <div className="textos flex flex-col w-1/2 mt-52">
             <h1 className="text-6xl textos__h1">Soy Joaquin Batallanes</h1>
             <h3 className="textos__h3 text-gray-500">
               Diseñador - Desarrollador web
             </h3>
-            <p className="pr-36 text-lg">
+            <p className="pr-36 text-lg text-center">
               Estas son algunas de las tecnoligías en las cuales trabajo
               actualmente. Te invito a conocer más de mi trabajo en mi portfolio
             </p>
 
-            <div className="w-full flex gap-10">
+            <div className="w-full flex gap-10 mt-8">
               <div>
                 <img src={html} alt="" srcset="" />
               </div>
@@ -80,12 +80,28 @@ function App() {
                 <img src={ai} alt="" srcset="" />
               </div>
             </div>
+
+            <div className="boton bg-black py-2 rounded-full text-white  mt-4">
+              <a href="" className="justify-center gap-2 flex">
+                <p>Mirá mi github</p>
+                <span>
+                  <i class="fi fi-brands-github"></i>
+                </span>
+              </a>
+            </div>
           </div>
 
           <div className="w-1/2">
-            <img className="h-screen" src={avatar} alt="" srcset="" />
+            <img className="h-screen absolute" src={avatar} alt="" srcset="" />
           </div>
         </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,64L60,90.7C120,117,240,171,360,170.7C480,171,600,117,720,85.3C840,53,960,43,1080,42.7C1200,43,1320,53,1380,58.7L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
       </div>
 
       <h1>hoola</h1>
