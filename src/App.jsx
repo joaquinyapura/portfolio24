e5f247aa22cf4aba4b1304f1804d12bd4e6b8c4c
Joaquin Macbook <joaquin@gjm.com>
2024-06-25 08:45:55 -0300

    actualizacion css y nueva sección

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import js from "./assets/js.png";
 import ps from "./assets/ps.png";
 import react from "./assets/react.png";
 import tw from "./assets/tw.png";
+import avatarsnow from "./assets/avatarsnow.png"
 
 function App() {
   return (
@@ -48,38 +49,91 @@ function App() {
             <h3 className="textos__h3 text-gray-500">
               Diseñador - Desarrollador web
             </h3>
-            <p className="pr-36 text-lg text-center">
+            <p className="pr-36  text-left">
               Estas son algunas de las tecnoligías en las cuales trabajo
               actualmente. Te invito a conocer más de mi trabajo en mi portfolio
             </p>
 
-            <div className="w-full flex gap-10 mt-8">
-              <div>
-                <img src={html} alt="" srcset="" />
+          <div className="slider overflow-hidden">
+            <div className="slidetrack">
+              <div classname="slide" >
+                <img src={html} alt="" srcset=""  />
               </div>
-              <div>
-                <img src={css} alt="" srcset="" />
+              <div classname="slide" >
+                <img src={css} alt="" srcset=""  />
               </div>
-              <div>
-                <img src={js} alt="" srcset="" />
+              <div classname="slide" >
+                <img src={js} alt="" srcset=""  />
               </div>
-              <div>
-                <img src={react} alt="" srcset="" />
+              <div classname="slide" >
+                <img src={react} alt="" srcset=""  />
               </div>
-              <div>
-                <img src={git} alt="" srcset="" />
+              <div classname="slide" >
+                <img src={git} alt="" srcset=""  />
               </div>
 
-              <div>
-                <img src={tw} alt="" srcset="" />
+              <div classname="slide" >
+                <img src={tw} alt="" srcset=""  />
               </div>
-              <div>
-                <img src={ps} alt="" srcset="" />
+              <div classname="slide" >
+                <img src={ps} alt="" srcset=""  />
               </div>
-              <div>
-                <img src={ai} alt="" srcset="" />
+              <div classname="slide" >
+                <img src={ai} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={html} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={css} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={js} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={react} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={git} alt="" srcset=""  />
+              </div>
+
+              <div classname="slide" >
+                <img src={tw} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={ps} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={ai} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={html} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={css} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={js} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={react} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={git} alt="" srcset=""  />
+              </div>
+
+              <div classname="slide" >
+                <img src={tw} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={ps} alt="" srcset=""  />
+              </div>
+              <div classname="slide" >
+                <img src={ai} alt="" srcset=""  />
               </div>
             </div>
+
+          </div>
 
             <div className="boton bg-black py-2 rounded-full text-white  mt-4">
               <a href="" className="justify-center gap-2 flex">
@@ -102,6 +156,15 @@ function App() {
             d="M0,64L60,90.7C120,117,240,171,360,170.7C480,171,600,117,720,85.3C840,53,960,43,1080,42.7C1200,43,1320,53,1380,58.7L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
           ></path>
         </svg>
+        <section className="acerca w-full flex grid-cols-2 px-20">
+        <div className="w-1/2">
+          <img src={avatarsnow} alt="" srcset="" />
+        </div>
+        <div className="w-1/2 text-right text-white">
+        <h2>Un poco acerca de mi!</h2>
+
+        </div>
+        </section>
       </div>
 
       <h1>hoola</h1>
